1a34815467e06744ed6ed3094db8e5715a3ed6b3
Josimar Junior <juniorcvlct17@gmail.com>
2022-07-07 11:59:58 -0400

    utiliza hooks para controlar valor de variaveis

--- src/App.jsx
+++ src/App.jsx
@@ -25,21 +25,6 @@ const posts = [
     ],
     publishedAt: new Date('2022-05-03 20:00:00'),
   },
-
-  {
-    id: 2,
-    author: {
-      avatarurl: 'https://github.com/guifferrari',
-      name: 'Guilherme Ferrari',
-      role: 'Developer'
-    },
-    content: [
-      { type: 'paragraph', content: 'Fala galeraa 👋'},
-      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
-      { type: 'link', content: 'jane.design/doctorcare'},
-    ],
-    publishedAt: new Date('2022-05-03 20:00:00'),
-  },
 ];
 
 export function App() {  
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,8 +5,15 @@ import styles from './Post.module.css';
 import { Comment } from './Comment.jsx';
 
 import { Avatar } from './Avatar';
+import { useState } from 'react';
+
 
 export function Post({ author, publishedAt, content }) {
+    const [comments, setComments] = useState([
+        1,
+        2,
+    ])
+    
     const publishedDateFormatted  = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
         locale: ptBR,
     })
@@ -15,6 +22,12 @@ export function Post({ author, publishedAt, content }) {
         locale: ptBR,
         addSuffix: true,
     })
+
+    function handleCreateNewComment() {
+        event.preventDefault()
+        
+        setComments([...comments, comments.length + 1]);
+    }
 
     return (
         <article className={styles.post}>
@@ -42,7 +55,7 @@ export function Post({ author, publishedAt, content }) {
                 })}
             </div>
 
-            <form className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
 
                 <textarea
@@ -55,9 +68,9 @@ export function Post({ author, publishedAt, content }) {
             </form>
 
             <div className={styles.commentList}>
-                <Comment />
-                <Comment />
-                <Comment />
+                {comments.map(comment => {
+                    return <Comment />
+                })}
             </div>
         </article>
     )
